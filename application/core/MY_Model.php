a12a985e8a74fa3fcd6b5676eb53252955684925
imtiazaminsajid <ambition.sajid@gmail.com>
2017-09-23 22:24:57 +0600

    sep 23

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -12,4 +12,35 @@ class MY_Model extends CI_Model {
     {
         parent::__construct();
     }
+
+    public function get($id = NULL, $single = FALSE){
+        if($id != NULL){
+            $filter = $this->_primary_filter;
+            $id = $filter($id);
+            $this->db->where($this->_primary_filter_key, $id);
+            $method = 'row';
+
+        }
+        elseif ($single == TRUE){
+            $method = 'row';
+        }
+        else {
+            $method = 'result';
+        }
+        /*if (!count($this->db->ar_orderby)){
+            $this->db->order_by($this->_order_by);
+        }*/
+        if(!count($this->db->order_by($this->_order_by))) {
+            $this->db->order_by($this->_order_by);
+        }
+        return $this->db->get($this->_table_name)->$method();
+    }
+
+    public function get_by($where, $single = FALSE){
+        $this->db->where($where);
+        return $this->get(NULL, $single);
+    }
+    public function save(){}
+    public function delete(){}
+
 }
