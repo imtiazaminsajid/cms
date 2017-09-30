5febc1572b6394f9597c85bafb47df2c4cfab27d
imtiazaminsajid <ambition.sajid@gmail.com>
2017-09-30 22:01:07 +0600

    sep 30 bootstrap and design

--- application/views/admin/components/page_head.php
+++ application/views/admin/components/page_head.php
@@ -5,5 +5,5 @@
     <title><?php echo $meta_title; ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Bootstrap -->
-<link href="<?php echo site_url('css/bootstrap.min.css'); ?>" rel="stylesheet" media="screen">
+<link href="<?php echo site_url('public_html/css/bootstrap.min.css'); ?>" rel="stylesheet" media="screen">
 </head>
